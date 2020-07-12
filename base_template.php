fcc16a6d9398ff35ee1bd6ed052cf0119dac54c3
glouder4 <34104456+glouder4@users.noreply.github.com>
2020-07-12 23:11:21 +0500

    Create base_template.php

---
+++ base_template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        [[Bootstrap4]]
+	<title>VatanPlus | [[*longtitle:default=`[[*pagetitle]]`]]</title>        
+</head>
+<body>
+	[[$header]]
+<div class="container">
+      <section>
+        <h1>[[*longtitle:default=`[[*pagetitle]]`]]</h1>
+        [[*content]]
+      </section>       
+</div>
+        [[$footer]]
+</body>
+</html>
