7ae25fa9864288e8c9a70ddd2c0006b6fb73f67a
glouder4 <34104456+glouder4@users.noreply.github.com>
2020-07-12 23:11:52 +0500

    Create Bootstrap4_snippet.php

---
+++ Bootstrap4_snippet.php
@@ -0,0 +1,6 @@
+<?php
+$modx->regClientCSS('/assets/components/bootstrap4/4.5.0/dist/css/bootstrap.min.css');
+$modx->regClientCSS('assets/components/templates/css/main.css');
+$modx->regClientStartupScript('assets/components/bootstrap4/4.5.0/dist/js/bootstrap.bundle.min.js');
+$modx->regClientStartupScript('assets/components/bootstrap4/4.5.0/dist/js/bootstrap.min.js');
+$modx->regClientStartupScript('assets/components/templates/js/all.min.js');
